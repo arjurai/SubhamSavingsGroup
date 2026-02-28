d25dea18b808c3843101a8ba5140d18333cfd61e
ARJU RAI <exposer-theatre-5y@icloud.com>
2026-03-01 02:41:21 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,25 @@
-self.addEventListener("install", e=>{
-e.waitUntil(
-caches.open("subham-cache").then(cache=>{
-return cache.addAll(["index.html"]);
-})
-);
+const CACHE_NAME = "subham-savings-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e=>{
-e.respondWith(
-caches.match(e.request).then(r=> r || fetch(e.request))
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
